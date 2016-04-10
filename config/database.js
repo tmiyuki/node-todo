3c3c2857f276f78957949009d1f8d7ec2331c7a7
tmiyuki <tmiyuki@gmail.com>
2016-04-10 15:19:45 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     #remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
     remoteUrl : 'mongodb://dummyuser:password@ec2-54-172-117-10.compute-1.amazonaws.com:27017/dummyDB',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    #localUrl: 'mongodb://localhost/meanstacktutorials'
 };
